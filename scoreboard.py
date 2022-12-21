02f56b0f85fc286dc58c8bdc3f755068096e427a
David McElhaney <104801768+dave1289@users.noreply.github.com>
2022-12-21 15:55:26 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 
 class ScoreBoard(Turtle):
-
+"""scoreboard class for scoreboard object in main.py game"""
 	def __init__(self):
 		super().__init__()
 		self.color("white")
@@ -12,13 +12,13 @@ class ScoreBoard(Turtle):
 		self.r_score = 0
 		self.display_score()
 
-
+# displays score at beginning of game when scoreboard object is created
 	def display_score(self):
 		self.goto(-100, 200)
 		self.write(self.l_score, align="center", font=("Courier", 60, "bold"))
 		self.goto(100, 200)
 		self.write(self.r_score, align="center", font=("Courier", 60, "bold"))
-
+# point keeping functionality for both players
 	def l_point(self):
 		self.l_score += 1
 		self.clear()
