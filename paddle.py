da526cf6f2661c8baeadd24ea72f1a2034242b2a
David McElhaney <104801768+dave1289@users.noreply.github.com>
2022-12-21 15:54:29 -0500

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-
+"""paddle turtle object for gameboard/player control
 	def __init__(self, position):
 		super().__init__()
 		self.pu()
@@ -11,7 +11,7 @@ class Paddle(Turtle):
 		self.color("white")
 		self.setx(position)
 		self.showturtle()
-
+# movement methods for up and down moving by increments of 20
 	def up(self):
 		new_y = self.ycor() + 20
 		self.goto(self.xcor(), new_y)
